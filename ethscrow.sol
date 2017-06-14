edfde6658bb009483b8d7416a5f084c7935b8856
Zac Mitton <zacmitton22@gmail.com>
2017-06-14 10:40:47 -0400

    showing holdings

--- ethscrow.sol
+++ ethscrow.sol
@@ -1,12 +1,49 @@
-contract Ethscrow{
-  Holding[] holdings;
-  mapping(uint )
+pragma solidity ^0.4.10;
+contract Ethscrow {
+  mapping (uint => Holding) public holdings;
+  uint public numHoldings;
 
-  struct Holding{
-    address origin;
-    address destination;
-    bool active;
+  struct Holding {
+    uint amount;
+    address owner1;
+    address owner2;
+    address owner1Sig;
+    address owner2Sig;
   }
 
+  function holdEth(address owner1, address owner2) payable{
+    numHoldings++;
+    holdings[numHoldings].amount = msg.value;
+    holdings[numHoldings].owner1 = owner1;
+    holdings[numHoldings].owner2 = owner2;
+  }
 
+  function signRelease(uint ID, address recipient){
+    if(holdings[ID].owner1 == msg.sender){
+      holdings[ID].owner1Sig = recipient;
+    }
+    if(holdings[ID].owner2 == msg.sender){
+      holdings[ID].owner2Sig = recipient;
+    }
+
+    if(releasable(ID, recipient)){
+      uint amount = holdings[ID].amount;
+      holdings[ID].amount = 0;
+      if(!holdings[ID].owner1Sig.send(amount)){ throw; }
+    }
+  }
+//read only
+
+  function releasable(uint ID, address recipient) constant returns(bool) {
+    return holdings[ID].owner1Sig == holdings[ID].owner2Sig 
+      && holdings[ID].owner1Sig != 0;
+  }
 }
+
+
+
+//rinkeby: 0x9c95a3751ab9b71e29b4dc715c15306067c405e7
+
+
+
+
--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 
 
-var balance;
+// var balance;
 
-function setStatus(message) {
-  var status = document.getElementById("status");
-  status.innerHTML = message;
-};
+// function setStatus(message) {
+//   var status = document.getElementById("status");
+//   status.innerHTML = message;
+// };
 
 function refreshBalances() {
-  var contract = escrow.deployed();
+  var contract = ethscrow.deployed();
   var balanceTable = document.getElementById('balances');
   balanceTable.innerHTML = "";
   for (var i = 0; i < web3.eth.accounts.length ; i++) {
@@ -27,15 +27,15 @@ function refreshBalances() {
   }
 };
 
-function holdCoin() {
-  var contract = escrow.deployed();
+function holdEth() {
   var amount = Math.floor(document.getElementById("amount").value * 1000000000000000000);
   var receiver = document.getElementById("receiver").value;
-  var signer_1 = document.getElementById("signer_1").value;
+  var owner1 = document.getElementById("owner1").value;
+  var owner2 = document.getElementById("owner2").value;
 
   setStatus("Initiating transaction... (please wait)");
 
-  contract.holdCoin(receiver, signer_1, {from: web3.eth.accounts[0], value: amount, gas: 1000000}).then(function(response) {
+  ethscrow.holdEth(receiver, owner1, owner2, {from: , value: amount, gas: 1000000}).then(function(response) {
     console.log(response);
 
     setStatus("Transaction complete!");
@@ -49,56 +49,87 @@ function holdCoin() {
   });
 };
 
-function signRelease() {
-  var contract = escrow.deployed();
-  var holdingNumber = Number(document.getElementById("holdingNumber").value);
+// function signRelease() {
+//   var contract = ethscrow.deployed();
+//   var holdingNumber = Number(document.getElementById("holdingNumber").value);
 
-  setStatus("Initiating transaction... (please wait)");
+//   setStatus("Initiating transaction... (please wait)");
 
-  contract.signRelease(holdingNumber, {from: web3.eth.accounts[0], gas: 1000000 }).then(function(response) {
-    console.log(response);
+//   contract.signRelease(holdingNumber, {from: web3.eth.accounts[0], gas: 1000000 }).then(function(response) {
+//     console.log(response);
 
-    setStatus("Transaction complete!");
-    refreshBalances();
-  }).catch(function(e) {
-    console.log(e);
-    setStatus("Error sending coin; see log.");
-  });
-};
+//     setStatus("Transaction complete!");
+//     refreshBalances();
+//   }).catch(function(e) {
+//     console.log(e);
+//     setStatus("Error sending coin; see log.");
+//   });
+// };
 
-window.onload = function() {
-  web3.eth.getAccounts(function(err, accs) {
-    if (err != null) {
-      alert("There was an error fetching your accounts.");
-      return;
+
+
+window.addEventListener('load', function(){
+  if(typeof web3 === 'undefined'){
+    web3 = new Web3(new Web3.providers.HttpProvider('https://rinkeby.infura.io'));
+    alert("Please install a wallet (like Metamask) to interact with the DAPP");
+  }else{
+
+    var ethscrowAddress = '0x9c95A3751aB9b71E29B4Dc715c15306067C405e7';
+    abi = [{"constant":true,"inputs":[{"name":"ID","type":"uint256"},{"name":"recipient","type":"address"}],"name":"releasable","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"ID","type":"uint256"},{"name":"recipient","type":"address"}],"name":"signRelease","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"numHoldings","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"holdings","outputs":[{"name":"amount","type":"uint256"},{"name":"owner1","type":"address"},{"name":"owner2","type":"address"},{"name":"owner1Sig","type":"address"},{"name":"owner2Sig","type":"address"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"owner1","type":"address"},{"name":"owner2","type":"address"}],"name":"holdEth","outputs":[],"payable":true,"type":"function"}];
+    ethscrow = web3.eth.contract(abi).at(ethscrowAddress);
+    // setTimeout(getNumHoldings, 500); //why do I need this timeout? no one knows :(
+    getHoldingData();
+
+    web3.version.getNetwork(function(e,r){ // rinkeby is "4"
+      if(r == 1){
+        console.log("MAINNET");
+      }else{
+        // alert("Please Connect Metamask to Ethereum Main Net");
+      }
+    })
+  }
+})
+
+
+getHoldingData = function(){
+  getNumHoldings();
+}
+getNumHoldings = function(){
+  ethscrow.numHoldings.call(function(e,num){
+    if(num){
+      console.log('NUM: ', num.toNumber());
+      // numHoldings = num.toNumber();
+      getHoldings(num.toNumber());
+    }else{
+      console.log(e)
     }
-
-    if (accs.length == 0) {
-      alert("Couldn't get any accounts! Make sure your Ethereum client is configured correctly.");
-      return;
+  })
+}
+getHoldings = function(numHoldings){
+  for (var i = 1; i <= numHoldings; i++) {
+    getHolding(i);
+  }
+}
+getHolding = function(ID){
+  ethscrow.holdings.call(ID, function(e,holding){
+    if(holding){
+      console.log('HOLDING: ', holding);
+      renderHolding(ID, holding);
+    }else{
+      console.log(e)
     }
-
-    refreshBalances();
-  });
+  })
+}
+renderHolding = function(ID, holding){
+  if(document.getElementById('holding'+ID)){
+    var tr = document.getElementById('holding'+ID)
+  }else{
+    var holdingTable = document.getElementById('holdingTable');
+    var tr = document.createElement("tr");
+    tr.id = holding+ID;
+    holdingTable.appendChild(tr);
+  }
+  tr.innerHTML = '<td>'+ holding[0] +'</td><td>'+ holding[1] +'</td><td>'+ holding[2] +'</td><td>'+ holding[3] +'</td><td>'+ holding[4] +'</td>';
 }
 
 
- // Added by Truffle bootstrap.                                
-
-// Supports Mist, and other wallets that provide 'web3'.      
-
-if (typeof web3 !== 'undefined') {                            
-
-  // Use the Mist/wallet provider.                            
-
-  window.web3 = new Web3(web3.currentProvider);               
-
-} else {                                                      
-
-  // Use the provider from the config.                        
-
-  window.web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545')); 
-
-}                                                             
-
-                                                              
